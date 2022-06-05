6fe829fb9bd328d3d0a713b1e3210c1cb2fb068c
Eder Soares <edersoares@me.com>
2022-06-05 14:50:14 -0300

    Init command

---
+++ src/Commands/InitCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Dex\Composer\PlugAndPlay\Commands;
+
+use Composer\Command\BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitCommand extends BaseCommand
+{
+    use CommandNaming, ComposerCreator;
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this->naming('plug-and-play:init');
+        $this->setDescription('Initialize plug and play plugin.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('<info>You are using Composer Plug and Play Plugin.</info>');
+
+        if ($input->getOption('plug-and-play-pretend')) {
+            return 0;
+        }
+
+        if (is_dir('packages')) {
+            $output->writeln('The [packages] directory already exists.');
+        } else {
+            mkdir('packages');
+        }
+
+        if (file_exists('packages/.gitignore')) {
+            $output->writeln('The [packages/.gitignore] file already exists.');
+        } else {
+            file_put_contents('packages/.gitignore', $this->gitignore());
+        }
+
+        if (file_exists('composer.json')) {
+            $output->writeln('The [packages/composer.json] file already exists.');
+        } else {
+            file_put_contents('packages/composer.json', $this->composer());
+        }
+
+        return 0;
+    }
+
+    private function gitignore(): string
+    {
+        $gitignore = <<<IGNORE
+*
+*/
+.gitignore
+IGNORE;
+
+        return trim($gitignore);
+    }
+
+    private function composer(): string
+    {
+        $composer = <<<COMPOSER
+{
+    "config": {
+        "allow-plugins": {
+            "dex/composer-plug-and-play": true
+        }
+    },
+    "extra": {
+        "composer-plug-and-play": {
+            "ignore": []
+        }
+    },
+    "minimum-stability": "dev",
+    "prefer-stable": true
+}
+COMPOSER;
+
+        return trim($composer);
+    }
+}
