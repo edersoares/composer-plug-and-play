13dfecacd9f726f612b48d960c2d8bc95d4275f0
Eder Soares <edersoares@me.com>
2022-09-23 14:52:28 -0300

    Add test

---
+++ tests/Unit/Commands/AddCommandTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Dex\Composer\PlugAndPlay\Tests\Unit\Commands;
+
+use Dex\Composer\PlugAndPlay\Tests\Application;
+use Dex\Composer\PlugAndPlay\Tests\TestCase;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class AddCommandTest extends TestCase
+{
+    protected $directory = __DIR__ . '/../../tmp';
+
+    protected function setUp(): void
+    {
+        $composer = json_encode([
+            'name' => 'dex/test',
+            'config' => [
+                'allow-plugins' => [
+                    'dex/composer-plug-and-play' => true,
+                    'dex/fake' => true,
+                ],
+            ],
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+        mkdir($this->directory);
+        file_put_contents($this->directory . '/composer.json', $composer);
+    }
+
+    protected function tearDown(): void
+    {
+        exec('rm -r ' . $this->directory);
+    }
+
+    public function test(): void
+    {
+        $application = new Application();
+        $input = new StringInput("plug-and-play:add -d {$this->directory} dex/extra");
+        $output = new BufferedOutput();
+
+        $application->doRun($input, $output);
+
+        $expected = json_encode([
+            'name' => 'dex/test',
+            'config' => [
+                'allow-plugins' => [
+                    'dex/composer-plug-and-play' => true,
+                    'dex/fake' => true,
+                ],
+            ],
+            'require' => [
+                'dex/extra' => '*',
+            ],
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+        $this->assertJsonStringEqualsJsonFile($expected, $this->directory . '/packages/composer.json');
+    }
+}
