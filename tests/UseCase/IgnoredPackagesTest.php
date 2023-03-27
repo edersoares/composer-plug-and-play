e1e575fd6ecc8891370996b5554fb1fe15cb295c
Eder Soares <edersoares@me.com>
2023-03-27 20:42:19 -0300

    Use case for ignored packages

---
+++ tests/UseCase/IgnoredPackagesTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dex\Composer\PlugAndPlay\Tests\UseCase;
+
+use Dex\Composer\PlugAndPlay\Tests\UseCase;
+
+class IgnoredPackagesTest extends UseCase
+{
+    protected function path(): string
+    {
+        return __DIR__ . '/../../fixtures/ignored-packages/';
+    }
+
+    public function testFactory(): void
+    {
+        $this->factory();
+
+        $this->assertGeneratedJsonEquals([
+            'extra' => [
+                'composer-plug-and-play' => [
+                    'ignore' => [
+                        'dex/ignore',
+                    ],
+                ],
+            ],
+            'config' => [
+                'allow-plugins' => true,
+            ],
+            'require' => [
+                'dex/not-ignore' => '@dev',
+            ],
+            'repositories' => [
+                [
+                    'type' => 'path',
+                    'url' => './packages/dex/not-ignore',
+                    'symlink' => true,
+                ],
+            ],
+        ]);
+    }
+}
