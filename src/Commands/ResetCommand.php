0e476d30f7aa5de7761c87c8b70ebee00b65d5d0
Eder Soares <edersoares@me.com>
2023-03-29 18:43:06 -0300

    Add `plug-and-play:reset` command

---
+++ src/Commands/ResetCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dex\Composer\PlugAndPlay\Commands;
+
+use Composer\Command\BaseCommand;
+use Dex\Composer\PlugAndPlay\PlugAndPlayInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResetCommand extends BaseCommand
+{
+    use ComposerCreator;
+    use CommandNaming;
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this->naming('plug-and-play:reset');
+        $this->setDescription('Reset plug and play files');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->outputPluginUse($output);
+
+        if (file_exists(PlugAndPlayInterface::FILENAME)) {
+            unlink(PlugAndPlayInterface::FILENAME);
+        }
+
+        $lock = str_replace('.json', '.lock', PlugAndPlayInterface::FILENAME);
+
+        if (file_exists($lock)) {
+            unlink($lock);
+        }
+
+        return 0;
+    }
+}
