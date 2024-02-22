c292085eb3a4d5a3107f67c6d98c8f0a71b0667e
Eder Soares <edersoares@me.com>
2024-02-22 09:27:56 -0300

    Set up and tear down bases

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,1 +1,16 @@
 <?php
+
+function fixture(string $fixture): mixed
+{
+    return test()->fixture($fixture);
+}
+
+function prepare(): mixed
+{
+    return test()->prepare();
+}
+
+function cleanup(): mixed
+{
+    return test()->cleanup();
+}
