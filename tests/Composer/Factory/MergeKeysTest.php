e47cc7e77ca9028b88c3d0718cba243c81257143
Eder Soares <edersoares@me.com>
2024-02-22 09:50:20 -0300

    Merge keys test

---
+++ tests/Composer/Factory/MergeKeysTest.php
@@ -0,0 +1,20 @@
+<?php
+
+beforeEach()
+    ->fixture('merge-keys')
+    ->prepare();
+
+afterEach()
+    ->cleanup();
+
+test('merge `composer.json` keys', function () {
+    $this->factory();
+
+    $this->assertGeneratedJsonEquals([
+        "minimum-stability" => "dev",
+        "prefer-stable" => false,
+        'config' => [
+            'allow-plugins' => true,
+        ],
+    ]);
+});
