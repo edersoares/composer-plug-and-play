4b4cb1a0408bdd8a29f64e425844f502badf38b6
jrbarros <joalissonricardo@gmail.com>
2025-06-12 09:05:41 -0300

    Create a dedicate exception

--- src/Commands/ComposerCreator.php
+++ src/Commands/ComposerCreator.php
@@ -6,7 +6,7 @@ use Composer\Composer;
 use Composer\Console\Application;
 use Composer\Json\JsonValidationException;
 use Dex\Composer\PlugAndPlay\Composer\Factory;
-use RuntimeException;
+use Dex\Composer\PlugAndPlay\Exceptions\ComposerCreationException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -21,7 +21,7 @@ trait ComposerCreator
      * @param bool      $required
      * @param bool|null $disablePlugins
      *
-     * @throws JsonValidationException|RuntimeException
+     * @throws JsonValidationException|ComposerCreationException
      *
      * @return Composer
      */
@@ -35,7 +35,7 @@ trait ComposerCreator
             if ($application instanceof Application) {
                 $composer = Factory::create($application->getIO(), null, $disablePlugins);
             } elseif ($required) {
-                throw new RuntimeException(
+                throw new ComposerCreationException(
                     'Could not create a Composer\Composer instance, you must inject ' .
                     'one if this command is not used with a Composer\Console\Application instance'
                 );
@@ -62,7 +62,7 @@ trait ComposerCreator
      *
      * @param bool|null $disablePlugins If null, reads --no-plugins as default
      * @param bool|null $disableScripts If null, reads --no-scripts as default
-     * @throws \RuntimeException
+     * @throws \Dex\Composer\PlugAndPlay\Exceptions\ComposerCreationException
      */
     public function requireComposer(bool $disablePlugins = null, bool $disableScripts = null): Composer
     {
